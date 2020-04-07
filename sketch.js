0e75af1c444296aebb7dc5f510d65afcaa69d2c4
wuguoxun <william.wu487@gmail.com>
2020-04-07 16:09:07 -0700

    setup

--- sketch.js
+++ sketch.js
@@ -1,7 +1,15 @@
 function setup() {
   createCanvas(400, 400);
+  background(220);
+
 }
 
 function draw() {
-  background(220);
+
+if(mouseIsPressed == true){
+  stroke(map(mouseX,0,600,0,255,true));
+  line(mouseX,mouseY,pmouseX,pmouseY);
+}
+
+
 }
