f247b0c88aff4a0b879f89de85e7bc421858dac6
wuguoxun <william.wu487@gmail.com>
2020-04-07 17:43:43 -0700

    final

--- sketch.js
+++ sketch.js
@@ -1,15 +1,54 @@
+let array = [];
+let backgroundColor = 200;
+
 function setup() {
-  createCanvas(400, 400);
-  background(220);
+  createCanvas(600, 600);
+  background(backgroundColor);
+
+  strokeWeight(5);
+  noFill();
+
 
 }
 
 function draw() {
 
-if(mouseIsPressed == true){
-  stroke(map(mouseX,0,600,0,255,true));
-  line(mouseX,mouseY,pmouseX,pmouseY);
+  if (mouseIsPressed == true) {
+    // stroke(map(mouseX, 0, 600, 0, 255, true));
+    // line(mouseX, mouseY, pmouseX, pmouseY);
+    backgroundColor -= 3;
+    background(backgroundColor);
+    array.push([mouseX, mouseY]);
+    beginShape();
+    for (let i = 0; i < array.length; i++) {
+      // line(array[i][0], array[i][1], array[i + 1][0], array[i + 1][1]);
+      curveVertex(array[i][0], array[i][1]);
+    }
+    endShape();
+  }
 }
 
+function keyTyped() {
 
+  if (key === 's') {
+    saveCanvas('drawing', 'png');
+  } else if (key === 'd') {
+    background(255);
+
+    beginShape();
+    for (let i = 0; i < array.length; i++) {
+      // line(array[i][0], array[i][1], array[i + 1][0], array[i + 1][1]);
+      curveVertex(array[i][0], array[i][1]);
+    }
+    endShape();
+
+  }
+
+  return false;
+}
+
+function mousePressed() {
+
+  array = [];
+  backgroundColor = 200;
 }
